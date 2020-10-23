61b03e3c5f1dc01cb83876fc8dad758ed03e1f2d
Tobenna-KA <kabanofor@yahoo.com>
2020-10-23 11:28:21 +0300

    A Simple calculator

---
+++ main.py
@@ -0,0 +1,32 @@
+git import re
+
+print("Our magical calculator")
+print("Type quit to exit\n")
+
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
+
+
+
+    if equation == "quit":
+        print("Goodbye, Human")
+        run = False
+    else:
+        equation = re.sub('[a-zA-z,.:()" "]', '', equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+        print("You typed", equation)
+while run:
+    performMath()
